9cbe679a69ff99ee3f0fc058fc79638d993ec689
SoliMark <24930156+SoliMark@users.noreply.github.com>
2025-07-30 23:50:29 +0800

    feat: add Task model definition

---
+++ internal/model/task.go
@@ -0,0 +1,14 @@
+package model
+
+import "time"
+
+type Task struct {
+	ID        uint   `gorm:"primaryKey"`
+	UserID    uint   `gorm:"not null;index"`
+	Title     string `gorm:"not null"`
+	Content   string
+	Status    string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time `gorm:"index"`
+}
